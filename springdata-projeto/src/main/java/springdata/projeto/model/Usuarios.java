6cc10692ab85ebe9d3707d66026d656cb7876da7
terc_wssantos <terc_wssantos@SECREGE-043094.uesc.net>
2023-12-05 10:17:02 -0300

    atualizado

---
+++ springdata-projeto/src/main/java/springdata/projeto/model/Usuarios.java
@@ -0,0 +1,51 @@
+package springdata.projeto.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GeneratorType;
+import org.hibernate.annotations.GenericGenerator;
+
+import net.bytebuddy.dynamic.loading.ClassReloadingStrategy.Strategy;
+
+@Entity
+public class Usuarios {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.SEQUENCE, generator = "usuario_sequence")
+	private Long id;
+	
+	private String nome;
+	private String login;
+	private String email;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
